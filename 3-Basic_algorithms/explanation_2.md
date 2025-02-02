640adb83cb10bea039b775c3700d68af64f40ffe
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2025-02-02 23:08:39 +0100

    Add files via upload

---
+++ 3-Basic_algorithms/explanation_2.md
@@ -0,0 +1,35 @@
+<!--
+Problem 2: Search in a Rotated Sorted Array
+
+Provide an explanation for your answer, clearly organizing your thoughts into 
+concise and easy-to-understand language.
+
+Focus on explaining the reasoning behind your decisions rather than giving a 
+detailed description of the code. For instance, why did you choose a particular 
+data structure? Additionally, discuss the efficiency of your solution in terms 
+of time and space complexity. If necessary, you can support your explanation 
+with code snippets or mathematical formulas. For guidance on how to write 
+formulas in markdown, refer to https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/writing-mathematical-expressions.
+-->
+If there was no rotation, the array would be sorted in ascending order.  
+As we have a rotation in the array, it means that we will have two  
+ascending order arrays, there are different scenarios in our binary search:
+
+1. The interval where we are search is not rotated and the sub array is sorted  
+in ascending order, in that case we can use normally the binary search to search the given value.
+
+2. The interval is rotated witch means that numbers inside are not sorted in  
+ascending order, so we have to continue our binary search until the sub array is sorted,  
+then we can decide if the value was found or not.
+
+We can use binary search to get into the right value in $O(log \ n)$ time complexity,  
+as each time there another recursion in the call stack the interval of values is halved  
+half.
+
+The space complexity is also $O(log \ n)$ as there will be $log \ n$ recursive calls in the  
+worst case, each recursive call adds another call frame, apart from that inside each call  
+stack the same space is always used, $O(1)$.
+
+**Conclusion**:
+* Time complexity: $O(log \ n)$  
+* Space complexity: $O(log \ n)$
---
+++ 3-Basic_algorithms/problem_5.py
@@ -0,0 +1,120 @@
+## Represents a single node in the Trie
+class TrieNode:
+    def __init__(self) -> None:
+        ## Initialize this node in the Trie
+        self.SIZE_ALPHABET = 26
+        self.child = [None] * self.SIZE_ALPHABET
+        self.wordEnd = False
+
+    def insert(self, char: str) -> None:
+        ## Add a child node in this Trie
+        if len(char) == 0:
+            return
+        char = char.lower()
+        index = ord(char[0]) - ord('a')
+        # insertion if node still not there
+        if self.child[index] == None:
+            self.child[index] = TrieNode()
+        # Test end of word
+        if len(char) == 1:
+            self.child[index].wordEnd = True
+        else:
+            self.child[index].insert(char[1:])
+
+        
+    def suffixes(self, suffix: str = '') -> list[str]:
+        """Recursive function that collects the suffix for 
+        all complete words below this point"""
+        words = list()
+        
+        if self.wordEnd and suffix:
+            words.append(suffix)
+
+        for i in range(self.SIZE_ALPHABET):
+            if self.child[i] is not None:
+                next_char = chr(ord('a') + i)
+                words.extend(self.child[i].suffixes(suffix + next_char))
+
+        return words
+
+## The Trie itself containing the root node and insert/find functions
+class Trie:
+    def __init__(self) -> None:
+        """Initialize this Trie (add a root node)"""
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """Add a word to the Trie"""
+        current = self.root
+        for char in word:
+            index = ord(char) - ord('a')
+            if current.child[index] is None:
+                current.child[index] = TrieNode()
+            current = current.child[index]
+        current.wordEnd = True
+
+    def find(self, prefix: str) -> TrieNode | None:
+        """Find the Trie node that represents this prefix"""
+        if len(prefix) == 0:
+            return None
+        node_search = self.root
+        for char in prefix:
+            index = ord(char) - ord('a')
+            if node_search.child[index] is None:
+                return None
+            node_search = node_search.child[index]
+        return node_search
+
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym", 
+    "fun", "function", "factory", 
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    MyTrie.insert(word)
+
+def f(prefix: str) -> None:
+    if prefix != '':
+        prefixNode = MyTrie.find(prefix)
+        if prefixNode:
+            print('\n'.join(prefixNode.suffixes()))
+        else:
+            print(prefix + " not found")
+    else:
+        print('')
+        
+
+# Edge case: Empty string
+prefixNode = MyTrie.find("")
+print('\n'.join(prefixNode.suffixes())) if prefixNode else print(prefixNode)
+assert(prefixNode == None)
+# Expected output: False
+
+# Normal case: Word present in the Trie
+prefixNode = MyTrie.find("ant")
+print('\n'.join(prefixNode.suffixes())) if prefixNode else print(prefixNode)
+suffixes_list = prefixNode.suffixes()
+assert(len(suffixes_list) == 3)
+assert("agonist" in suffixes_list)
+assert("hology" in suffixes_list)
+assert("onym" in suffixes_list)
+# Expected output: True
+
+# Test case: Word present in the Trie, no suffixes
+prefixNode = MyTrie.find("function")
+print('\n'.join(prefixNode.suffixes())) if prefixNode else print(prefixNode)
+suffixes_list = prefixNode.suffixes()
+assert(len(suffixes_list) == 0)
+# Expected output: True
+
+# # Normal case: Prefix of a word present in the Trie
+# prefixNode = MyTrie.find("fun")
+# print('\n'.join(prefixNode.suffixes())) if prefixNode else print(prefixNode)
+# # Expected output: True
+# print("\n")
+
+# # Normal case: Prefix of a word present in the Trie
+# prefixNode = MyTrie.find("f")
+# print('\n'.join(prefixNode.suffixes())) if prefixNode else print(prefixNode)
+# # Expected output: True
